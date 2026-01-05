10fdd0a3db4edbb220a0aca423f139ef6dc18bba
Kriti Khadka Chhetri <chhetrikriti06@gmail.com>
2026-01-05 17:16:36 +0545

    Add files via upload

---
+++ CHP7/forloopiterate.py
@@ -0,0 +1,14 @@
+## For loop with lists
+l = [1, 4, 6, 234, 6, 764]
+
+for i in l:
+    print(i)
+## For loop with tuples 
+
+t = (23, 45, 67, 89)
+for j in t:
+    print(j)
+## For loop with strings   
+s = "Hello"
+for c in s:
+    print(c)
---
+++ CHP7/listusingwhileloop.py
@@ -0,0 +1,7 @@
+l = [1, "Kriiti", 3.5, True, 5]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i += 1
